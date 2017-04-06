7e084cbea412d39864b26d7856899965fcf03a78
Tom Banister <tgbanister@gmail.com>
2017-04-06 13:34:26 +0100

    Remove the now ignored directory node_modules

---
+++ index.js
@@ -0,0 +1,18 @@
+var util = require('util'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var socket = require('socket.io-client')('http://leedsdocktravel.azurewebsites.net/');
+
+  socket.on('pushBoatData', function(data) {
+      const string = JSON.stringify(data.twee, null, 4);
+        res.write(string);
+        res.end();
+        socket.disconnect();
+  });
+}).listen(8000);
+
+
+/* server started */
+util.puts('> hello world running on port 8000');
